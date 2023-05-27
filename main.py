56a448d7b906af6ba689f2f17ebf064a6e6fcf91
Domen Bencak <domen.bencak@student.um.si>
2023-05-27 23:03:23 +0200

    Noticed performance drop in face detection accuracy. Deleted a few images and modified program

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from deepface import DeepFace
 
 cap = cv2.VideoCapture(0)
 
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 400)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 600)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
 counter = 0
 
@@ -18,30 +18,34 @@ reference_imgs = {}
 domen_folder_path = "domen"
 elon_folder_path = "elon"
 
-# Get all image files for Domen with extensions .jpg, .jpeg, and .png
+# Load reference images for Domen
 domen_image_files = [file for file in os.listdir(domen_folder_path) if file.lower().endswith((".jpg", ".jpeg", ".png"))]
-# Load the images for Domen and add them to the reference_imgs dictionary under the person's name
 reference_imgs["Domen"] = [cv2.imread(os.path.join(domen_folder_path, file)) for file in domen_image_files]
 
-# Get all image files for Elon with extensions .jpg, .jpeg, and .png
+# Load reference images for Elon
 elon_image_files = [file for file in os.listdir(elon_folder_path) if file.lower().endswith((".jpg", ".jpeg", ".png"))]
-# Load the images for Elon and add them to the reference_imgs dictionary under the person's name
 reference_imgs["Elon"] = [cv2.imread(os.path.join(elon_folder_path, file)) for file in elon_image_files]
 
-# Add more persons with their folder paths if needed
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+def detect_faces(frame):
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    return faces
 
 def check_face(frame):
     global recognized_person
     try:
-        for person, images in reference_imgs.items():
-            for reference_img in images:
-                if DeepFace.verify(frame, reference_img.copy())['verified']:
-                    recognized_person = person
-                    return
+        faces = detect_faces(frame)
+        if len(faces) > 0:
+            for person, images in reference_imgs.items():
+                for reference_img in images:
+                    if DeepFace.verify(frame, reference_img.copy())['verified']:
+                        recognized_person = person
+                        return
         recognized_person = "Unknown"
     except ValueError:
         recognized_person = "Unknown"
-
 
 while True:
     ret, frame = cap.read()
