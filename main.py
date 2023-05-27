f766f3f7b437a5078cad3448c77fe032bc9db689
Domen Bencak <domen.bencak@student.um.si>
2023-05-27 23:30:30 +0200

    Added functionality that if there is a match, it takes and saves a picture of a person. This way our database is growing by itself. Limited this to one per session. *Could be making detection worse, not tested enough*

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
 counter = 0
+was_saved = False  # Flag to track if a picture was already saved in the current run
 
 recognized_person = "Unknown"
 
@@ -18,34 +19,48 @@ reference_imgs = {}
 domen_folder_path = "domen"
 elon_folder_path = "elon"
 
-# Load reference images for Domen
+# Get all image files for Domen with extensions .jpg, .jpeg, and .png
 domen_image_files = [file for file in os.listdir(domen_folder_path) if file.lower().endswith((".jpg", ".jpeg", ".png"))]
+# Load the images for Domen and add them to the reference_imgs dictionary under the person's name
 reference_imgs["Domen"] = [cv2.imread(os.path.join(domen_folder_path, file)) for file in domen_image_files]
 
-# Load reference images for Elon
+# Get all image files for Elon with extensions .jpg, .jpeg, and .png
 elon_image_files = [file for file in os.listdir(elon_folder_path) if file.lower().endswith((".jpg", ".jpeg", ".png"))]
+# Load the images for Elon and add them to the reference_imgs dictionary under the person's name
 reference_imgs["Elon"] = [cv2.imread(os.path.join(elon_folder_path, file)) for file in elon_image_files]
 
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-def detect_faces(frame):
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
-    return faces
+# Add more persons with their folder paths if needed
+
+def save_frame(frame, person):
+    global was_saved
+    if not was_saved:
+        folder_path = os.path.join(person.lower(), ".")
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+
+        filename = f"{person}_{counter}.jpg"
+        file_path = os.path.join(folder_path, filename)
+        cv2.imwrite(file_path, frame)
+        print(f"Saved frame for {person} in {file_path}")
+        was_saved = True
+    else:
+        pass
+        #print("Already saved")
 
 def check_face(frame):
     global recognized_person
     try:
-        faces = detect_faces(frame)
-        if len(faces) > 0:
-            for person, images in reference_imgs.items():
-                for reference_img in images:
-                    if DeepFace.verify(frame, reference_img.copy())['verified']:
-                        recognized_person = person
-                        return
+        for person, images in reference_imgs.items():
+            for reference_img in images:
+                if DeepFace.verify(frame, reference_img.copy())['verified']:
+                    recognized_person = person
+                    threading.Thread(target=save_frame, args=(frame.copy(), person)).start()
+                    return
         recognized_person = "Unknown"
     except ValueError:
         recognized_person = "Unknown"
+
 
 while True:
     ret, frame = cap.read()
@@ -61,7 +76,10 @@ while True:
         if recognized_person == "Unknown":
             cv2.putText(frame, recognized_person, (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
         else:
-            cv2.putText(frame, recognized_person, (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+            if recognized_person == "Domen":
+                cv2.putText(frame, recognized_person, (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+            elif recognized_person == "Elon":
+                cv2.putText(frame, recognized_person, (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
 
         cv2.imshow("Camera", frame)
 
